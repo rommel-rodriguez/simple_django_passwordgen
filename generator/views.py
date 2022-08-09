a1b397160a4f894ae4d861558824d391644515bb
Rommel Rodriguez Perez <alrodperez@gmail.com>
2022-08-09 09:07:38 -0500

    The prototype is working and generating passwords
    Rough prototype working:
        - Created home and password temaplates
        - Created functions inside views.py that use those templates
        - Named the path object in urls.py so that can I access it inside a
        template, useful to set form targets
        - The logic for password generation was implemented inside views.py,
        inside the password function.

--- generator/views.py
+++ generator/views.py
@@ -1,11 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import random
+import string
 # Create your views here.
+
+def build_character_set():
+    """ @brief Returns a list of characters acceptable for a password
+    """
+    characters = list(string.ascii_letters)
+    characters.extend(string.punctuation)
+    return characters
 
 
 def home(request):
     # NOTE: This should return a properly formatted HTTP Response
     # or, of course, and object that spits such a thing.
     # return 'This should be an HTTP Response'
-    return HttpResponse('<h1>This should be an HTTP Response</h1>')
+    return render(
+            request,
+            'generator/home.html',
+            dict(password='orgohgoaj31234')
+            )
+
+
+def password(request):
+    # NOTE: This should return a properly formatted HTTP Response
+    # or, of course, and object that spits such a thing.
+    # return 'This should be an HTTP Response'
+    password_chars = build_character_set()
+    length = int(request.POST['length'])
+    proto_password = ''
+    # password = ""
+    for char in range(length):
+        proto_password += random.choice(password_chars)
+
+
+    return render(
+            request,
+            'generator/password.html',
+            dict(password=proto_password)
+            )
